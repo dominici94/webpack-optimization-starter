0ad788b6670fcce38512ceeadd58c7f4b96de2de
dominici94 <mattiadominici94@gmail.com>
2022-12-06 10:58:36 +0100

    css module webpack

---
+++ dist/main.9bf5174d2e7633c480d7.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatewebpack_optimization_starter"]("main",{
+
+/***/ "./src/js/ui.js":
+/*!**********************!*\
+  !*** ./src/js/ui.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (37:2)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n|   setTimeout(function () {\\n|     const notificationWlwmwnr\\n>   });\\n| }\\n| \");\n\n//# sourceURL=webpack://webpack-optimization-starter/./src/js/ui.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("372194d667f0694ae981")
+/******/ })();
+/******/ 
+/******/ }
+);
--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const config = {
   entry: "./src/js/index.js",
   output: {
     path: path.resolve(__dirname, "../dist"),
-    clean: true,
+    // clean: true,
   },
   module: {
     rules: [
@@ -24,6 +25,12 @@ const config = {
       filename: "index.html",
       template: "src/template.html",
     }),
+    // new CleanWebpackPlugin({
+    //   cleanOnceBeforeBuildPatterns: [
+    //     "**/*",
+    //     path.join(process.cwd(), "build/**/*"),
+    //   ],
+    // }),
   ],
 };
 
